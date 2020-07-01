1acc250c98c6439ca6392760c1491114d31ba448
Rogerson Jean-Charles <Rogerson253@Gmail.com>
2020-06-30 20:09:12 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.urlencoded({ extended : true }));
+app.use(express.json());
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`app is listening on port ${PORT}`);
+});
