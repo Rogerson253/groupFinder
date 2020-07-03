95d466a8e09e38840c2576db158629d3fc23873b
naritchaya louis <naritchayalouis@gmail.com>
2020-07-02 19:59:41 -0500

    added get route

--- controllers/friendsController.js
+++ controllers/friendsController.js
@@ -2,8 +2,20 @@ var express = require("express");
 
 var router = express.Router();
 
+// Import the model 
 
+var user = require("../models/user.js");
 
+// Create all our routes
+app.get("/", function(req, res) {
+  user.all(function(data) {
+    var userData = {
+      user: data
+    };
+    console.log(userData);
+    res.render("index", userData);
+  });
+});
 
 app.post("/api/new", function(req, res) {
 
