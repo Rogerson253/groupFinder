7282f0f978f59ed96491f88dfaa61ba3c68334e5
Andrea Fenderson <andrea_fenderson@ymail.com>
2020-07-02 19:27:32 -0500

    created model folder, user.js, and user model

---
+++ client/public/models/user.js
@@ -0,0 +1,16 @@
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define("user", {
+      
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    });
+    return User;
+  };
