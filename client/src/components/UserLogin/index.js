cbd91cf486683438999029f4afe2195af73b15cf
Andrea Fenderson <andrea_fenderson@ymail.com>
2020-07-18 10:47:08 -0500

    username displays on the page, rearranged code, fixed components

--- client/src/components/UserLogin/index.js
+++ client/src/components/UserLogin/index.js
@@ -4,24 +4,34 @@ import { Component } from 'react';
 
 
 class Login extends Component{
-    constructor(props){
-        super(props); 
-        this.state = {}
-    }
+    state = {
+        username: "",
+        password: "",   
+    };
+    // constructor(props){
+    //     super(props); 
+    //     this.state = {}
+    
 
 updateInputField = (element) =>{
     const name = element.target.name;
     const value = element.target.value;
     
     this.setState({[name]: value})
+};
 
-}    
-submit = () =>{
+loginSubmit = () =>{
     const userName = this.state.userId;
     const password = this.state.passId;
 //    API.createNewUser 
    console.log(userName, password)
 
+
+
+this.setState({
+    username: "",
+    password: "",
+});
 }
 
 render() {
@@ -29,8 +39,7 @@ render() {
         <div>
             <span> {this.state.userId}</span>
         </div>
-    )
-}
+    )};
 
 
 
@@ -40,24 +49,34 @@ render() {
     render(){
         return(
             <Fragment>
+                <h1>Log In Page</h1>
+            <p>Hello {this.state.userId} 
+            </p>
                   <div className="col">
         <form className="login">
-            <h1>Log In Page</h1>
-            <p>Username:</p>
-            <input name= "userId" type="username" onChange= {this.updateInputField} id="username-input" placeholder="Username"/>
+
+            <input 
+            name= "userId" 
+            type="username"
+            onChange= {this.updateInputField} id="username-input" placeholder="Username"/>
+            
             <p>Password:</p>
-            <input name = "passId" type="password" onChange= {this.updateInputField} id="password-input" placeholder="Password"/>
+            <input 
+            name = "passId" 
+            type="password" 
+            onChange= {this.updateInputField} id="password-input" placeholder="Password"/>
             <br/>
-            <button type="submit" onClick= {()=> this.submit()}>Submit</button>
+            <button type="submit" onClick= {()=> this.loginSubmit()}>Submit</button>
             <p>No Account? <a href="/signup.html">Sign Up</a></p>
         </form>
                 </div>
 
             </Fragment>
-        )
-    }
+        )};
 
 }
+
+
 
 export default Login
 
