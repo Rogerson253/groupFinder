2be437fe1caa13d2353e974703d9a3f98d5c026b
Kiara Mahan <kiaramahan@gmail.com>
2020-07-18 12:43:53 -0500

    added questions

--- client/src/components/Questions/Quiz.jsx
+++ client/src/components/Questions/Quiz.jsx
@@ -25,6 +25,98 @@ class Quiz extends Component{
             <ListGroup.Item action onClick={alertClicked}>3</ListGroup.Item>
             <ListGroup.Item action onClick={alertClicked}>4</ListGroup.Item>
             </ListGroup>
+
+            <h4>#2. The Krusty Krab pizza is, in fact, the pizza for you and me.</h4>
+
+            <ListGroup horizontal>
+            <ListGroup.Item action onClick={alertClicked}>1</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>2</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>3</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>4</ListGroup.Item>
+            </ListGroup>
+
+            <h4>#3. Book Ginny is light years better than movie Ginny.</h4>
+
+            <ListGroup horizontal>
+            <ListGroup.Item action onClick={alertClicked}>1</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>2</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>3</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>4</ListGroup.Item>
+            </ListGroup>
+
+
+            <h4>#4. Someday we will be able to hear the words "what is love" without them immediately being followed in our minds by the words "baby don't hurt me."</h4>
+
+            <ListGroup horizontal>
+            <ListGroup.Item action onClick={alertClicked}>1</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>2</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>3</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>4</ListGroup.Item>
+            </ListGroup>
+
+            <h4>#5. White Claws are pretty good, ok?</h4>
+
+            <ListGroup horizontal>
+            <ListGroup.Item action onClick={alertClicked}>1</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>2</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>3</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>4</ListGroup.Item>
+            </ListGroup>
+
+            <h4>#6. 2014 Forest Hills Drive was ok.</h4>
+
+            <ListGroup horizontal>
+            <ListGroup.Item action onClick={alertClicked}>1</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>2</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>3</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>4</ListGroup.Item>
+            </ListGroup>
+
+            <h4>#7. Britney Spears' 2007 meltdown was a leap in cultural evolution.</h4>
+
+            <ListGroup horizontal>
+            <ListGroup.Item action onClick={alertClicked}>1</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>2</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>3</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>4</ListGroup.Item>
+            </ListGroup>
+
+            <h4>#8. To date, there has been no conclusive evidence gathered proving in any way that the bird is the word.</h4>
+
+            <ListGroup horizontal>
+            <ListGroup.Item action onClick={alertClicked}>1</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>2</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>3</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>4</ListGroup.Item>
+            </ListGroup>
+
+            <h4>#9. #PlutoIsAPlanet.</h4>
+
+            <ListGroup horizontal>
+            <ListGroup.Item action onClick={alertClicked}>1</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>2</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>3</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>4</ListGroup.Item>
+            </ListGroup>
+
+
+            <h4>#10. We should grant French fries citizenship.</h4>
+
+            <ListGroup horizontal>
+            <ListGroup.Item action onClick={alertClicked}>1</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>2</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>3</ListGroup.Item>
+            <ListGroup.Item action onClick={alertClicked}>4</ListGroup.Item>
+            </ListGroup>
+
+
+
+
+
+
+
+
+
             </Fragment>
         )
 
