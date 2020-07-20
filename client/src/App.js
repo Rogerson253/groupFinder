aea9922440f9b93d4e12e94de9315551f6cf9255
Andrea Fenderson <andrea_fenderson@ymail.com>
2020-07-20 18:53:01 -0500

    added quiz import and browser router hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "./components/UserLogin";
-// import Quiz from "./components/Questions";
+import Quiz from "./components/Questions";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <Route exact path="/" component={Login} />
       <Route exact path="/questions" component={Quiz} />
-      {/* <Quiz /> */}
+     
     </div>
     </Router>
   );
