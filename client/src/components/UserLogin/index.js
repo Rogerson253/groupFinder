27f9013981bc97c164960098a958fd38e66d414f
Rogerson Jean-Charles <Rogerson253@Gmail.com>
2020-07-20 17:36:28 -0500

    created functions that console log form inputted data and reads data in database

--- client/src/components/UserLogin/index.js
+++ client/src/components/UserLogin/index.js
@@ -1,82 +1,90 @@
-import React, { Fragment } from 'react'
-import { Component } from 'react';
-// import API from '../../routes/API';
+import React, { Fragment } from "react";
+import { Component } from "react";
+import API from "../utils/API";
 
-
-class Login extends Component{
-    state = {
-        username: "",
-        password: "",   
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
     };
-    // constructor(props){
-    //     super(props); 
-    //     this.state = {}
-    
+  }
 
-updateInputField = (element) =>{
-    const name = element.target.name;
-    const value = element.target.value;
-    
-    this.setState({[name]: value})
-};
+  //   updateInputField = (element) => {
+  //     const name = element.target.name;
+  //     const value = element.target.value;
 
-loginSubmit = () =>{
-    const userName = this.state.userId;
-    const password = this.state.passId;
-//    API.createNewUser 
-   console.log(userName, password)
+  //     this.setState({ [name]: value });
+  //   };
 
+  getUserData = () => {
+    API.getUser()
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-this.setState({
-    username: "",
-    password: "",
-});
-}
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { username, password } = this.state;
+    API.saveUser({
+      username,
+      password,
+    })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-render() {
+  render() {
+    const { username, password } = this.state;
     return (
-        <div>
-            <span> {this.state.userId}</span>
-        </div>
-    )};
+      <Fragment>
+        <h1>Log In Page</h1>
+        {/* <p>Hello {this.state.username}</p> */}
+        <div className="col">
+          <form className="login" onSubmit={this.handleSubmit}>
+            <input
+              name="username"
+              type="text"
+              value={username}
+              onChange={this.handleChange}
+              id="username-input"
+              placeholder="Username"
+            />
 
-
-
-
-
-
-    render(){
-        return(
-            <Fragment>
-                <h1>Log In Page</h1>
-            <p>Hello {this.state.userId} 
-            </p>
-                  <div className="col">
-        <form className="login">
-
-            <input 
-            name= "userId" 
-            type="username"
-            onChange= {this.updateInputField} id="username-input" placeholder="Username"/>
-            
             <p>Password:</p>
-            <input 
-            name = "passId" 
-            type="password" 
-            onChange= {this.updateInputField} id="password-input" placeholder="Password"/>
-            <br/>
-            <button type="submit" onClick= {()=> this.loginSubmit()}>Submit</button>
-            <p>No Account? <a href="/signup.html">Sign Up</a></p>
-        </form>
-                </div>
-
-            </Fragment>
-        )};
-
+            <input
+              name="password"
+              type="text"
+              value={password}
+              onChange={this.handleChange}
+              id="password-input"
+              placeholder="Password"
+            />
+            <br />
+            <button onClick={this.getUserData} type="submit">
+              Submit
+            </button>
+            <p>
+              No Account? <a href="/signup.html">Sign Up</a>
+            </p>
+          </form>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-
-
-export default Login
-
+export default Login;
