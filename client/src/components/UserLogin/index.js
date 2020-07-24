aff129490f516470402f376ea48d4e7fb5bca2ef
naritchaya louis <naritchayalouis@gmail.com>
2020-07-24 18:32:38 -0500

    done with styling sign up and sign in pages

--- client/src/components/UserLogin/index.js
+++ client/src/components/UserLogin/index.js
@@ -2,6 +2,9 @@ import React, { Fragment } from "react";
 import { Component } from "react";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
+import { Button, Container, Jumbotron, Col, Row} from 'react-bootstrap';
+import "./style.css";
+
 
 class Login extends Component {
   constructor() {
@@ -62,10 +65,22 @@ class Login extends Component {
     const { username, password } = this.state;
     return (
       <Fragment>
-        <h1>Log In Page</h1>
+        <body>
+        <Jumbotron className="jumbotron" style={{textAlign: "center", fontSize:"20%", padding:"3%", backgroundColor:"#808080"}}>
+        <div className="appName">
+        <h1>The Friend Finder</h1>
+        </div>
+        </Jumbotron> 
+
+        <Container className ="container" style={{display:"block", marginLeft:"auto", marginRight:"auto",backgroundColor:"#FFFF99", width: "30%", padding:"1%", marginTop:"2%"}} >
+        <h2>Sign Up Page</h2>
+        <Row>
+        <Col>
+      
         {/* <p>Hello {this.state.username}</p> */}
         <div className="col">
           <form className="login" onSubmit={this.handleSubmit}>
+          <p>Username:</p>
             <input
               name="username"
               type="text"
@@ -74,6 +89,7 @@ class Login extends Component {
               id="username-input"
               placeholder="Username"
             />
+
             <p>Password:</p>
             <input
               name="password"
@@ -85,15 +101,25 @@ class Login extends Component {
             />
             <br />
             <Link to="/questions">
-            <button onClick={this.getUserData} type="submit">
+
+            <Button style={{ fontWeight:"bolder", marginBottom:"3%", marginTop:"1%", backgroundColor:"greenyellow"}} as="input" type="submit" value="Submit" onClick={this.getUserData} />{' '}
+
+
+            {/* <button onClick={this.getUserData} type="submit">
               Submit
-            </button>
+            </button> */}
+
+
             </Link>
             <p>
               No Account? <Link to = "/UserSignUp"> <button type="submit">Sign Up</button></Link>
             </p>
           </form>
         </div>
+        </Col>
+        </Row>
+        </Container>
+        </body>
       </Fragment>
     );
   }
